c7ec064107f131ad65440831b277033f6d14fc27
Bruno Fernandes <brunocpf@outlook.com>
2024-12-16 22:34:50 -0300

    enhance custom keyboard layout and add loading spinner styling

--- src/components/custom-keyboard.tsx
+++ src/components/custom-keyboard.tsx
@@ -27,13 +27,13 @@ export function CustomKeyboard() {
   };
 
   return (
-    <Card className="mt-3 flex bg-gray-100 p-1 dark:bg-zinc-900 dark:[&_.hg-button]:!border-black dark:[&_.hg-button]:!bg-zinc-600 dark:active:[&_.hg-button]:!bg-zinc-500 [&_.hg-row:nth-child(2):not(&_.keyboard-theme-2_.hg-row)]:!px-2 [&_.hg-row:nth-child(3)]:!px-5 [&_.keyboard-theme]:bg-transparent">
+    <Card className="mt-3 flex w-96 max-w-[100vw] bg-gray-100 p-1 dark:bg-zinc-900 [&_.hg-button]:text-nowrap dark:[&_.hg-button]:!border-black dark:[&_.hg-button]:!bg-zinc-600 dark:active:[&_.hg-button]:!bg-zinc-500 [&_.hg-row:nth-child(2):not(&_.keyboard-theme-2_.hg-row)]:!px-2 [&_.hg-row:nth-child(3)]:!px-8 [&_.keyboard-theme]:bg-transparent">
       {loading && (
         <div className="absolute inset-0 grid place-items-center bg-gray-100 dark:bg-zinc-900">
           <div className="h-20 w-20 animate-spin rounded-full border-b-2 border-t-2 border-zinc-400 dark:border-zinc-800"></div>
         </div>
       )}
-      <div>
+      <div className="flex-1">
         <Keyboard
           onInit={() => setLoading(false)}
           theme={"hg-theme-default hg-layout-default keyboard-theme"}
